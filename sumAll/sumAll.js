492589f8abb990b0f163ae9c27ee829a8f2e550c
Armin <armin.hamp@minerva.kgi.edu>
2020-05-16 14:54:09 +0200

    Added solution for sumAll()

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    let start=Math.min(n1,n2);
+    let end=Math.max(n1,n2);
+    let sum=0;
+    if (n1<0 || n2<0){
+        return 'ERROR'
+    }
+    if (typeof(n1)!='number' || typeof(n2)!='number'){
+        return 'ERROR'
+    }
+    for (let i=start;i<=end;i++){
+        sum+=i;
+    }
+    return sum
 }
 
 module.exports = sumAll
