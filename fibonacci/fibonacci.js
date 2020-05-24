781597a4becf111a9ac24485341cb408abf94997
Armin <armin.hamp@minerva.kgi.edu>
2020-05-24 10:55:55 +0200

    Added solutions to fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0){
+        return 'OOPS';
+    }
+    array=[];
+    while (array.length<n){
+        if (array.length==0){
+            array.push(1);
+        }
+        topush=0
+        array.slice(Math.max(array.length - 2, 0)).forEach(element => {
+            topush+=element;
+        });
+        array.push(topush);
+    }
+    return array[array.length - 1]
 }
 
 module.exports = fibonacci
