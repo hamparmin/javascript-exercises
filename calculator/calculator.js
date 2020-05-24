c5a8fb33ef2c6bd65e119f9c020182733f4244d5
Armin <armin.hamp@minerva.kgi.edu>
2020-05-24 10:15:33 +0200

    Added calculator solutions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,24 +1,41 @@
-function add () {
-	
+function add (a,b) {
+	return a+b
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return a-b
+}
+function sum (array) {
+	sum=0
+	array.forEach(element => {
+		sum+=element;
+	});
+	return sum
 }
 
-function sum () {
-	
+function multiply (array) {
+	total=1;
+	array.forEach(element => {
+		total*=element;
+	});
+	return total
 }
 
-function multiply () {
-	
+function power(a,b) {
+	return Math.pow(a,b)
 }
 
-function power() {
-	
-}
-
-function factorial() {
+function factorial(a) {
+	if (a==0 || a==1){
+		return 1
+	}
+	total=a;
+	array=[...Array(a).keys()];
+	array.shift();
+	array.forEach(element => {
+		total*=element;
+	});
+	return total
 	
 }
 
