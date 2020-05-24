89ac79cfe1e3054927c4471a6088108aa364b26d
Armin <armin.hamp@minerva.kgi.edu>
2020-05-24 11:05:20 +0200

    Added solutions for getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    arr=[];
+    obj.forEach(element => {
+        arr.push(element.title)
+        
+    });
+    return arr
 }
 
 module.exports = getTheTitles;
